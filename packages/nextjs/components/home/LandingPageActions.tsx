d544cfa50d0576514023e2b0e9755ef84ae939e5
David Hawig <davidhawig@gmail.com>
2026-05-31 11:20:29 +0200

    perf(nextjs): move public pages out of app shell

--- packages/nextjs/components/home/LandingPageActions.tsx
+++ packages/nextjs/components/home/LandingPageActions.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import styles from "./LandingPageActions.module.css";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
-import { HumanSignInButton } from "~~/components/shared/HumanSignInButton";
 import { DOCS_AI_ROUTE } from "~~/constants/routes";
-import { LANDING_HUMAN_CTA_LABEL } from "~~/lib/home/humanSignInRoute";
+import { HUMAN_SIGN_IN_DISCOVER_ROUTE, LANDING_HUMAN_CTA_LABEL } from "~~/lib/home/humanSignInRoute";
 
 export function LandingPageActions() {
   return (
     <div className="mt-6 flex flex-wrap justify-center gap-3 lg:justify-start">
-      <HumanSignInButton className={`btn btn-primary ${styles.cta} ${styles.primary}`}>
+      <Link
+        href={HUMAN_SIGN_IN_DISCOVER_ROUTE}
+        prefetch={false}
+        className={`btn btn-primary ${styles.cta} ${styles.primary}`}
+      >
         <span>{LANDING_HUMAN_CTA_LABEL}</span>
         <span className={styles.arrow} aria-hidden="true">
           <ChevronRightIcon className="h-5 w-5 text-current" />
         </span>
-      </HumanSignInButton>
+      </Link>
       <Link href={DOCS_AI_ROUTE} prefetch={false} className={`btn whitespace-nowrap ${styles.cta} ${styles.secondary}`}>
         <span>For Agents</span>
         <span className={styles.arrow} aria-hidden="true">
