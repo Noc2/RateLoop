c9da59fcda166df494b83686eb41ff6e7d38d4c7
David Hawig <davidhawig@gmail.com>
2026-06-21 15:34:44 +0200

    fix(nextjs): provide public wallet action contexts

--- packages/nextjs/app/(public)/layout.tsx
+++ packages/nextjs/app/(public)/layout.tsx
@@ -1,10 +1,18 @@
 import { PublicShell } from "~~/components/PublicShell";
 import { RateLoopWalletProviders } from "~~/components/RateLoopWalletProviders";
+import { TermsAcceptanceModal } from "~~/components/legal/TermsAcceptanceModal";
+import { WalletFundingProvider } from "~~/components/shared/WalletFundingProvider";
+import { TermsAcceptanceProvider } from "~~/contexts/TermsAcceptanceContext";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <RateLoopWalletProviders>
-      <PublicShell>{children}</PublicShell>
+      <TermsAcceptanceProvider>
+        <WalletFundingProvider>
+          <PublicShell>{children}</PublicShell>
+        </WalletFundingProvider>
+        <TermsAcceptanceModal />
+      </TermsAcceptanceProvider>
     </RateLoopWalletProviders>
   );
 }
