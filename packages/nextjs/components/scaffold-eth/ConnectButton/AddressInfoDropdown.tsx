67bbfce779dd0cf5a4f31a3ef1e110aa167d37e8
David Hawig <davidhawig@gmail.com>
2026-07-01 17:27:52 +0200

    Stack mobile wallet balances

--- packages/nextjs/components/scaffold-eth/ConnectButton/AddressInfoDropdown.tsx
+++ packages/nextjs/components/scaffold-eth/ConnectButton/AddressInfoDropdown.tsx
@@ -364,7 +364,7 @@ export const AddressInfoDropdown = ({
         <li className="px-3 py-2" data-testid="wallet-connected">
           <div className="flex items-start gap-3">
             <BlockieAvatar address={checkSumAddress} size={24} ensImage={ensAvatar} />
-            <div className="min-w-0 flex-1">
+            <div className="min-w-0 flex flex-1 flex-col gap-1">
               <ClaimRewardsButton
                 className="mb-1"
                 includeFrontendRewards={false}
