ae9c9b5714bae5f9e27d6fcd2a60d7bc07f9e889
David Hawig <davidhawig@gmail.com>
2026-07-10 16:53:53 +0200

    fix: normalize keeper Docker boolean flags

--- packages/keeper/docker-entrypoint.sh
+++ packages/keeper/docker-entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 set -eu
 
-if [ "${KEEPER_CORRELATION_SNAPSHOTS_ENABLED:-false}" = "true" ] &&
-  [ "${KEEPER_CORRELATION_ARTIFACT_STORAGE:-file}" = "file" ]; then
+snapshots_enabled=$(
+  printf '%s' "${KEEPER_CORRELATION_SNAPSHOTS_ENABLED:-false}" |
+    tr '[:upper:]' '[:lower:]' |
+    sed 's/^[[:space:]]*//; s/[[:space:]]*$//'
+)
+
+case "$snapshots_enabled" in
+1 | true | yes | on) snapshots_enabled=true ;;
+*) snapshots_enabled=false ;;
+esac
+
+if [ "$snapshots_enabled" = "true" ] && [ "${KEEPER_CORRELATION_ARTIFACT_STORAGE:-file}" = "file" ]; then
   artifact_dir="${KEEPER_CORRELATION_SNAPSHOT_STORAGE_DIR:-correlation-artifacts}"
   case "$artifact_dir" in
     /*) ;;
